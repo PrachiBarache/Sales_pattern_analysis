ba222409858aa3680144f5c33e84c9a7a96be493
PrachiBarache <prachi.barache@city.ac.uk>
2026-02-03 04:56:02 +0000

    revenuecustomer

--- notebooks/eda.py
+++ notebooks/eda.py
@@ -5,3 +5,8 @@ import seaborn as sns
 from src.datacleaning import data_load_to_clean
 
 df = data_load_to_clean("data/raw/online_retail.csv")
+# Revenue concentration
+customer_revenue = df.groupby("CustomerID")["TotalPrice"].sum()
+customer_revenue.describe()
+sns.histplot(customer_revenue, bins=50)
+plt.title("Customer Revenue Distribution")
